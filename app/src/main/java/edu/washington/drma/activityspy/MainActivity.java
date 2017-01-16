1fdc5c076a34b937ba539204631aafa1cbe35402
Duncan <duncanandrew@live.com>
2017-01-15 22:24:51 -0800

    assignment finished

--- app/src/main/java/edu/washington/drma/activityspy/MainActivity.java
+++ app/src/main/java/edu/washington/drma/activityspy/MainActivity.java
@@ -2,6 +2,7 @@ package edu.washington.drma.activityspy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +12,35 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate event fired");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart event fired");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume event fired");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause event fired");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop event fired");
+    }
+
+    @Override
+    protected void onDestroy(){
+        Log.e(TAG, "We’re going down, Captain!");
     }
 }
